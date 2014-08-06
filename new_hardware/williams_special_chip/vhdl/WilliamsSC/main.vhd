9482a33389e81c6813f23cbcb400677f8c30a0e3
Matt Win7 <no email>
2014-08-06 08:00:05 -0600

    updated to match Blitter PCB design

--- new_hardware/williams_special_chip/vhdl/WilliamsSC/main.vhd
+++ new_hardware/williams_special_chip/vhdl/WilliamsSC/main.vhd
@@ -31,17 +31,31 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity main is
     Port (
-				A : inout  STD_LOGIC_VECTOR(15 downto 14);
-           B : inout  STD_LOGIC_VECTOR(15 downto 0);
-           C : inout  STD_LOGIC_VECTOR(15 downto 0));
+				AH : out std_logic_vector(15 downto 8);
+				AL : inout std_logic_vector(7 downto 0);
+				BA_BS_N : in std_logic;
+				CS_N : in std_logic;
+				D : inout std_logic_vector(7 downto 0);
+				E_N : in std_logic;
+				HALT_N : in std_logic;
+				HTCF_N : out std_logic;
+				MHZ4 : in std_logic;
+				R_WN : inout std_logic;
+				RESET_N : in std_logic;
+				TCF_N : in std_logic;
+				U_LN : in std_logic;
+				WINH_N : out std_logic);
 end main;
 
 architecture Behavioral of main is
 
 begin
 
-A(15 downto 14) <= "11";
-B <= "1111111111111111";
-C <= "1111111111111111";
+AH(15 downto 8) <= "11111111";
+AL(7 downto 0) <= "11111111";
+D(7 downto 0) <= "11111111";
+HTCF_N <= '1';
+R_WN <= '1';
+WINH_N <= '1';
 
 end Behavioral;
